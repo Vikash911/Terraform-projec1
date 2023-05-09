b19fbcfa7154714e8c25e67703760e40082044eb
Vikash Bhade <vikashbhade911@gmail.com>
2023-05-09 12:54:00 +0530

    new

--- providers.tf
+++ providers.tf
@@ -7,8 +7,6 @@ terraform {
   }
 }
 provider "aws" {
-         access_key = "AKIAQNTLOUGAIULBQZSB"
-         secret_key = "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"
 	 region = "us-east-1"
         #profile = "Vikash"
         }
